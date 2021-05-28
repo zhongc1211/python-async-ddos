14f8544f8fca941c3a092210fe1dcdf13be3fd28
zack <zack.zhong@sleek.com>
2021-05-28 12:35:07 +0800

    fix project id

--- kubectl-init.sh
+++ kubectl-init.sh
@@ -4,4 +4,4 @@
 echo $GKE_SA_KEY > .gcloud-api-key.json
 docker login -u _json_key --password-stdin https://gcr.io < .gcloud-api-key.json
 gcloud auth activate-service-account --key-file .gcloud-api-key.json
-gcloud container clusters get-credentials $GKE_CLUSTER --region=$GKE_ZONE --project $GKE_PROJECT
+gcloud container clusters get-credentials $GKE_CLUSTER --region=$GKE_ZONE --project $PROJECT_ID
